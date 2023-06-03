6f8dc8999f7eccc59717ebb727809fad99c33744
Lucas <web.lucasvieira@gmail.com>
2023-06-03 18:37:56 -0300

    tests UserService implementation

--- src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
@@ -1,12 +1,15 @@
 package com.client.ws.rasmooplus.service;
 
 import com.client.ws.rasmooplus.dto.UserDto;
+import com.client.ws.rasmooplus.exception.BadRequestException;
+import com.client.ws.rasmooplus.exception.NotFoundException;
 import com.client.ws.rasmooplus.model.jpa.User;
 import com.client.ws.rasmooplus.model.jpa.UserType;
 import com.client.ws.rasmooplus.repository.jpa.UserRepository;
 import com.client.ws.rasmooplus.repository.jpa.UserTypeRepository;
 import com.client.ws.rasmooplus.service.implementations.UserServiceImplementation;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,8 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
@@ -29,17 +32,27 @@ class UserServiceTest {
 
     @InjectMocks
     private UserServiceImplementation userService;
-    @Test
-    void given_create_when_idIsNotNullAndUserIsFound_then_returnUserCreated(){
 
-        UserDto dto = new UserDto();
+    private UserDto dto;
+
+    @BeforeEach
+    public void loadUser() {
+        dto = new UserDto();
+        dto.setId(1L);
         dto.setEmail("felipe@email.com");
         dto.setCpf("11111111111");
         dto.setUserTypeId(1L);
+    }
+
+    @Test
+    void given_create_when_idIsNotNullAndUserIsFound_then_returnUserCreated(){
 
         UserType userType = new UserType(1L, "Aluno", "Aluno da plataforma");
 
-        when(userTypeRepository.findById(1L)).thenReturn(Optional.of(userType));
+        when(userTypeRepository.findById(1L))
+                .thenReturn(Optional.of(userType));
+
+        dto.setId(null);
 
         User user = new User();
         user.setEmail(dto.getEmail());
@@ -49,7 +62,31 @@ class UserServiceTest {
         user.setUserType(userType);
         doReturn(user).when(userRepository).save(Mockito.any(User.class));
 
-        Assertions.assertEquals(user,userService.create(dto)); ;
+        Assertions.assertEquals(user,userService.create(dto));
+
+        Mockito.verify(userTypeRepository, times(1)).findById(1L);
+        Mockito.verify(userRepository, times(1)).save(Mockito.any(User.class));
     }
 
+    @Test
+    void given_create_when_idIsNotNull_then_throwBadRequestException() {
+
+
+        Assertions.assertThrows(BadRequestException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(0)).findById(any());
+        verify(userRepository, times(0)).save(any());
+    }
+
+    @Test
+    void given_create_when_idIsNullAndUserTypeIsNotFound_then_throwNotFoudException(){
+
+        dto.setId(null);
+        when(userTypeRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoundException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(1)).findById(1L);
+        verify(userRepository, times(0)).save(any());
+    }
 }
